035770a019d911e010fe58186d76ea1a12611ca5
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-05-02 11:40:53 +0100

    setting up dev ops for my blog

---
+++ www/admin/includes/dashboard_header.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TEST</title>
+	<link rel="stylesheet" type="text/css" href="../styles/styles.css">
+</head>
+
+<body>
+	<section>
+		<div class="mast">
+			<h1>T<span>SSB</span></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="adminHome.php"<?php currNav("adminHome.php"); ?>>View Products</a></li>
+					<li><a href="addProduct.php"<?php currNav("addProduct.php"); ?>>Add Products</a></li>
+					<li><a href="category.php"<?php currNav("category.php"); ?>>View Categories</a></li>
+					<li><a href="add_category.php"<?php currNav("add_category.php"); ?>>Add Categories</a></li>
+					<li><a href="logout.php"<?php currNav("logout.php"); ?>>logout</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
